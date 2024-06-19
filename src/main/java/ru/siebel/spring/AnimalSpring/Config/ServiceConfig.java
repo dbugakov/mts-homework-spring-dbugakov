b3e66c41c5919d1c1ea512d12182c1aa8a8399cc
Danil Bugakov <dbugakov@mtsbank.ru>
2024-06-19 16:54:21 +0300

    HW-7:Проект переделан на Spring Boot JPA, добавлена инициализация БД через liquibase.

--- src/main/java/ru/siebel/spring/AnimalSpring/Config/ServiceConfig.java
+++ src/main/java/ru/siebel/spring/AnimalSpring/Config/ServiceConfig.java
@@ -1,16 +1,13 @@
 package ru.siebel.spring.AnimalSpring.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import ru.siebel.spring.AnimalSpring.Service.CreateAnimalServiceImpl;
 
 @Configuration
 public class ServiceConfig {
 
-    @Bean(name = "serviceFromConfig")
+    /*@Bean(name = "serviceFromConfig")
     @Scope("prototype")
     public CreateAnimalServiceImpl CreateAnimalServiceImpl() {
         return new CreateAnimalServiceImpl();
-    }
+    }*/
 }
--- src/main/java/ru/siebel/spring/AnimalSpring/Service/CreateAnimalServiceImpl.java
+++ src/main/java/ru/siebel/spring/AnimalSpring/Service/CreateAnimalServiceImpl.java
@@ -1,82 +1,39 @@
 package ru.siebel.spring.AnimalSpring.Service;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.siebel.spring.AnimalSpring.Api.Model.Animal;
+import org.springframework.transaction.annotation.Transactional;
+import ru.siebel.spring.AnimalSpring.Api.Repository.AnimalRepository;
 import ru.siebel.spring.AnimalSpring.Api.Service.CreateAnimalService;
-import ru.siebel.spring.AnimalSpring.Model.AbstractAnimal;
-import ru.siebel.spring.AnimalSpring.Util.FileUtil;
-
-import java.util.*;
+import ru.siebel.spring.AnimalSpring.Model.Animal;
 
 /**
  * Класс CreateAnimalServiceImpl, реализует {@link CreateAnimalService}.
  */
 @Service
 @Data
-@Scope("prototype")
+@Transactional
+@RequiredArgsConstructor
 public class CreateAnimalServiceImpl implements CreateAnimalService {
-    Map<String, List<Animal>> resultMap;
-    Animal animal;
-    String stringForFileFill = "";
-    @Value("#{'${animal.cat.names}'.split(',')}")
-    List<String> listOfNames;
 
-    /**
-     * Функция создания десяти уникальных животных.
-     * Переопределён из {@link CreateAnimalService}.
-     *
-     * @return HashMap, где ключ - название класса, значение - массив Animal
-     */
+    @Autowired
+    private final AnimalRepository animalRepository;
+
+    @Autowired
+    private final SearchServiceImpl searchService;
+
     @Override
-    public Map<String, List<Animal>> createAnimals() {
-        System.out.println("createAnimals - do while");
-        resultMap = new HashMap<>();
-        int i = 0;
-        do {
-            fillResultMap();
-            fillFile(i + 1);
-            i++;
-        } while (i < 10);
-        System.out.println(resultMap);
-        return resultMap;
-    }
-
-    /**
-     * Функция создания уникальных животных в заданном количестве.
-     * Перегружен из метода {@link CreateAnimalServiceImpl#createAnimals}.
-     *
-     * @return HashMap, где ключ - название класса, значение - массив Animal
-     */
-    public Map<String, List<Animal>> createAnimals(int animalCount) {
-        System.out.println("createAnimals - for");
-        resultMap = new HashMap<>();
-        for (int i = 0; i < animalCount; i++) {
-            fillResultMap();
-            fillFile(i + 1);
+    public void deleteAnimal(Long animalId) {
+        if (animalRepository.existsById(animalId)) {
+            animalRepository.deleteById(animalId);
         }
-
-        System.out.println(resultMap);
-        return resultMap;
     }
 
-    private void fillResultMap() {
-        animal = AbstractAnimal.getRandomAnimal();
-        Random random = new Random();
-        animal.setName(listOfNames.get(random.nextInt(listOfNames.size())));
-        if (resultMap.containsKey(animal.getClass().getSimpleName())) {
-            resultMap.get(animal.getClass().getSimpleName()).add(animal);
-        } else {
-            resultMap.put(animal.getClass().getSimpleName(), new ArrayList<>());
-            resultMap.get(animal.getClass().getSimpleName()).add(animal);
-        }
-        System.out.println(animal);
-    }
-
-    private void fillFile(int cnt) {
-        stringForFileFill += cnt + " " + animal.getBreed() + " " + animal.getName() + " " + animal.getCost() + " " + animal.getBirthDate() + "\n";
-        FileUtil.fileChannelWrite(stringForFileFill, "src/main/resources/animals/logData.txt");
+    @Override
+    public Long addAnimal(Animal animal) {
+        animalRepository.save(animal);
+        return animal.getId();
     }
 }
